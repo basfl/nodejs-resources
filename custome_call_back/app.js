7f984bda0583c7850b93304537b7660bd95baf23
unknown <jupiter85838@yahoo.com>
2019-07-23 19:52:11 -0400

    custome call back

---
+++ custome_call_back/app.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const path = require('path');
+const p = path.join(__dirname, 'data', 'data.json')
+console.log(p)
+
+const getProductsFromFile = (callback) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            callback([]);
+        } else {
+
+            callback(JSON.parse(fileContent));
+        }
+    });
+};
+const findOne = (id, callback) => {
+    getProductsFromFile(products => {
+        const product = products.find(p => p.id = id)
+        callback(product)
+    })
+}
+
+getProductsFromFile(products => {
+    console.log("got result back from getProductsFromFile ")
+    console.log(products)
+
+})
+console.log("dont wait go ahead till we ger result from getProductsFromFile")
+findOne("123", product => {
+    console.log("got result back fromfindOne")
+    console.log(product)
+})
+console.log("dont wait go ahead till we ger result from findOne")
+
